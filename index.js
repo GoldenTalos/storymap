0382d9345f6e51834ad0d38f01798649f3d509e4
Sophie Dong <sophiedong2007@gmail.com>
2023-02-06 10:18:31 -0500

    a

---
+++ index.js
@@ -0,0 +1,29 @@
+const N = 99;
+const x = 0;
+const y = 0;
+const z = 0;
+const a = 0;
+
+function change() {
+  while (N > 0) {
+    if ((N) => 25) {
+      N -= 25;
+      x += 1;
+    }
+    elseif (N => 10) {
+        N-=10;
+        y+=1;
+    }
+    elseif (N => 5) {
+        N-=5;
+        z+=1;
+    }
+    elseif (N => 1) {
+        N-=1;
+        a+=1;
+    }        
+  }
+  let total = x+y+z+a
+  console.log(total)
+console.log(x, y, z, a)
+}
