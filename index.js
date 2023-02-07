daa7a9d2b956587dc4c42c20e570fbd17eb5de9f
Sophie Dong <sophiedong2007@gmail.com>
2023-02-07 09:57:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const N = 99;
-const x = 0;
+const N = x * 100;
+const q = 0;
 const y = 0;
 const z = 0;
 const a = 0;
@@ -9,21 +9,18 @@ function change() {
     if ((N) => 25) {
       N -= 25;
       x += 1;
+    } else if ((N) => 10) {
+      N -= 10;
+      y += 1;
+    } else if ((N) => 5) {
+      N -= 5;
+      z += 1;
+    } else if ((N) => 1) {
+      N -= 1;
+      a += 1;
     }
-    elseif (N => 10) {
-        N-=10;
-        y+=1;
-    }
-    elseif (N => 5) {
-        N-=5;
-        z+=1;
-    }
-    elseif (N => 1) {
-        N-=1;
-        a+=1;
-    }        
   }
-  let total = x+y+z+a
-  console.log(total)
-console.log(x, y, z, a)
+  let total = x + y + z + a;
+  console.log(total);
+  console.log(x, y, z, a);
 }
